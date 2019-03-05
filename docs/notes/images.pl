69b2f2dfa4e8e3d61629ece72d8f29744c239bbf
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-03-04 17:24:28 -0700

    html version of programming notes

---
+++ docs/notes/images.pl
@@ -0,0 +1,472 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate images original text with physical files.
+
+
+$key = q/0110R_4R_3R_2R_1R_0C_4C_3C_2C_1C_0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.14ex; vertical-align: -0.45ex; " SRC="|."$dir".q|img9.svg"
+ ALT="$0110R_4R_3R_2R_1R_0C_4C_3C_2C_1C_0$">|; 
+
+$key = q/0R_4R_3Y_2Y_1Y_0R_2R_1R_0C_4C_3C_2C_1C_0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.14ex; vertical-align: -0.45ex; " SRC="|."$dir".q|img8.svg"
+ ALT="$0R_4R_3Y_2Y_1Y_0R_2R_1R_0C_4C_3C_2C_1C_0$">|; 
+
+$key = q/100xxxxx;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.70ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img20.svg"
+ ALT="$100xxxxx$">|; 
+
+$key = q/1110R_4R_3R_2R_1R_0C_4C_3C_2C_1C_0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.14ex; vertical-align: -0.45ex; " SRC="|."$dir".q|img11.svg"
+ ALT="$1110R_4R_3R_2R_1R_0C_4C_3C_2C_1C_0$">|; 
+
+$key = q/128times96;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img19.svg"
+ ALT="$128\times96$">|; 
+
+$key = q/128times96times256;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img34.svg"
+ ALT="$128\times96\times256$">|; 
+
+$key = q/16times16;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img28.svg"
+ ALT="$16\times16$">|; 
+
+$key = q/1R_4R_3Y_2Y_1Y_0R_2R_1R_0C_4C_3C_2C_1C_0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.14ex; vertical-align: -0.45ex; " SRC="|."$dir".q|img10.svg"
+ ALT="$1R_4R_3Y_2Y_1Y_0R_2R_1R_0C_4C_3C_2C_1C_0$">|; 
+
+$key = q/1times;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img32.svg"
+ ALT="$1\times$">|; 
+
+$key = q/256times192;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img5.svg"
+ ALT="$256\times192$">|; 
+
+$key = q/256times192times256;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img27.svg"
+ ALT="$256\times192\times256$">|; 
+
+$key = q/2times;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img29.svg"
+ ALT="$2\times$">|; 
+
+$key = q/320times256;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img22.svg"
+ ALT="$320\times256$">|; 
+
+$key = q/32times192;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img13.svg"
+ ALT="$32\times192$">|; 
+
+$key = q/32times24;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img7.svg"
+ ALT="$32\times24$">|; 
+
+$key = q/352times240;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img44.svg"
+ ALT="$352\times240$">|; 
+
+$key = q/352times288;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img43.svg"
+ ALT="$352\times288$">|; 
+
+$key = q/40times30;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img25.svg"
+ ALT="$40\times30$">|; 
+
+$key = q/40times32;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img21.svg"
+ ALT="$40\times32$">|; 
+
+$key = q/4times4;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img33.svg"
+ ALT="$4\times4$">|; 
+
+$key = q/4times;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img30.svg"
+ ALT="$4\times$">|; 
+
+$key = q/512times192;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img14.svg"
+ ALT="$512\times192$">|; 
+
+$key = q/640times256;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img24.svg"
+ ALT="$640\times256$">|; 
+
+$key = q/64times24;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img17.svg"
+ ALT="$64\times24$">|; 
+
+$key = q/64times32;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img16.svg"
+ ALT="$64\times32$">|; 
+
+$key = q/80times30;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img26.svg"
+ ALT="$80\times30$">|; 
+
+$key = q/80times32;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img23.svg"
+ ALT="$80\times32$">|; 
+
+$key = q/8times1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img12.svg"
+ ALT="$8\times1$">|; 
+
+$key = q/8times8;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img6.svg"
+ ALT="$8\times8$">|; 
+
+$key = q/8times;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.90ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img31.svg"
+ ALT="$8\times$">|; 
+
+$key = q/C_0R_4R_3Y_2Y_1Y_0R_2R_1R_0C_5C_4C_3C_2C_1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.14ex; vertical-align: -0.45ex; " SRC="|."$dir".q|img18.svg"
+ ALT="$C_0R_4R_3Y_2Y_1Y_0R_2R_1R_0C_5C_4C_3C_2C_1$">|; 
+
+$key = q/Rightarrow;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.06ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$\Rightarrow$">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslash00-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img52.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/00-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslash20-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img53.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/20-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslash40-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img54.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/40-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslash60-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img55.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/60-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslash80-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img56.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/80-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslasha0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img57.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/a0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashc0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img58.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/c0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcb00-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img60.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cb00-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcb20-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img61.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cb20-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcb40-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img62.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cb40-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcb60-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img63.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cb60-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcb80-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img64.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cb80-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcba0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img65.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cba0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcbc0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img66.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cbc0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashcbe0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img67.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/cbe0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashdd00-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img68.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/dd00-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashdd60-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img69.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/dd60-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashdd90-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img70.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/dd90-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashe0-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img59.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/e0-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashed00-opcode.csv}{par{*ZXSpectrumNextextension{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img71.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/ed00-opcode.csv}
+\par
+* ZX Spectrum Next extension
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashed50-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img72.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/ed50-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashed90-opcode.csv}{par{*ZXSpectrumNextextension{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img73.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/ed90-opcode.csv}
+\par
+* ZX Spectrum Next extension
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashfd00-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img74.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/fd00-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashfd60-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img75.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/fd60-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{scriptsize{{{csvautotabular{opcodeslashfd90-opcode.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img76.svg"
+ ALT="\begin{table}\centering\scriptsize
+\csvautotabular{opcode/fd90-opcode.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{small{{{csvautotabular{copperslashidealparam.csv}{par{{raggedr{**Thefractionalpartisspecifiedindotclocks(NOOPinstructions).{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img46.svg"
+ ALT="\begin{table}\centering\small
+\csvautotabular{copper/idealparam.csv}
+\par
+\ra...
+... The fractional part is specified in dot clocks (NOOP instructions).
+\end{table}">|; 
+
+$key = q/{table}{centering{{tiny{{{csvautotabular{copperslashvtime.csv}{par{{raggedright{--Linecompareisoutofrange{par{*ULAVBLANKinterrupt.{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img42.svg"
+ ALT="\begin{table}\centering\tiny
+\csvautotabular{copper/vtime.csv}
+\par
+\raggedright - Line compare is out of range
+\par
+* ULA VBLANK interrupt.
+\end{table}">|; 
+
+$key = q/{table}{centering{{tiny{{{csvautotabular{portsslashports.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img51.svg"
+ ALT="\begin{table}\centering\tiny
+\csvautotabular{ports/ports.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashaftermaxcmp.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img40.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/aftermaxcmp.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashcontrolmodes.csv}{par{{raggedright*Thecontrolmodenamesusedinthisguidedifferfromtheofficialnames.{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img49.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/controlmodes.csv}
+\par
+\ragged...
+...ntrol mode names used in this guide differ from
+the official names.
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashhtime.csv}{par{{raggedright{--Dotclockcompareisoutofrange.{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img41.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/htime.csv}
+\par
+\raggedright - Dot clock compare is out of range.
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashidealres.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img45.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/idealres.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashinstrbit.csv}{par{{raggedright{H6bticallinecompare{par{R7bitNextregister0x00..0x7F{par{D8bitdata{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img47.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/instrbit.csv}
+\par
+\raggedrigh...
+...line compare
+\par
+R 7 bit Next register 0x00..0x7F
+\par
+D 8 bit data
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashmaxhcmp.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img39.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/maxhcmp.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashperline.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img37.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/perline.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashpersec.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img38.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/persec.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashregbit.csv}{par{{raggedright{D8bitdata{par{I11bitindex{par{C2bitcontrol{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img48.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/regbit.csv}
+\par
+\raggedright D 8 bit data
+\par
+I 11 bit index
+\par
+C 2 bit control
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{copperslashvideotiming.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img50.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{copper/videotiming.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{videoslashflash.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img3.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{video/flash.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{videoslashhires.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img15.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{video/hires.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{videoslashpalfmt.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img4.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{video/palfmt.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{videoslashvideolayering.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img1.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{video/videolayering.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{zx128mm.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img35.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{zx128mm.csv}
+\end{table}">|; 
+
+$key = q/{table}{centering{{{csvautotabular{zxndmaslashregisters.csv}{table};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img36.svg"
+ ALT="\begin{table}\centering
+\csvautotabular{zxndma/registers.csv}
+\end{table}">|; 
+
+1;
+
