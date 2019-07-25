6140a08ea74d904f4b343004080e44487eff7cb5
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-07-25 07:57:07 -0400

    fix memory allocation problem

--- zxnftp/client/call_get.c
+++ zxnftp/client/call_get.c
@@ -12,6 +12,7 @@ char *call_get(char *param, int *fsize) {
   int16_t len;
   struct timespec s, e;
   double time;
+  int y, x;
   
   clock_gettime(CLOCK_REALTIME, &s);
   nettxln("GT");
@@ -23,11 +24,19 @@ char *call_get(char *param, int *fsize) {
     return NULL;
   }
   *fsize=atoi(buf);
+  sprintf(buf, "%d bytes\n", *fsize);
+  waddstr(win, buf);
   if (*fsize>data_sz) {
-    data_sz*=2;
-    data=(char *)realloc(data, data_sz);
+    data_sz=*fsize;
+    free(data);
+    data=(char *)malloc(data_sz);
   }
   for(rsize=0; rsize<*fsize; rsize+=len) {
+    sprintf(buf, "%d", rsize);
+    waddstr(win, buf);
+    wrefresh(win);
+    getyx(win, y,x);
+    wmove(win, y, 0);
     nettxln("RR");
     netrx(data+rsize, &len, BLKSZ);
   }
@@ -37,7 +46,7 @@ char *call_get(char *param, int *fsize) {
   }
   clock_gettime(CLOCK_REALTIME, &e);
   time=e.tv_sec-s.tv_sec+(e.tv_nsec-s.tv_nsec)*1e-9;
-  sprintf(buf, "%d b, 0.2f s, %0.2f bps\n", *fsize, time, 8**fsize/time);
+  sprintf(buf, "%d b, %0.2f s, %0.2f bps\n", *fsize, time, 8**fsize/time);
   waddstr(win, buf);
   return data;
 }
