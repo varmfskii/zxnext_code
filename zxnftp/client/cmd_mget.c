30e63df26ca717e1fd74dc3390a07b73bae4485b
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-08-04 14:55:03 -0400

    remove zxnftp (now just in tools)

--- zxnftp/client/cmd_mget.c
+++
@@ -1,8 +0,0 @@
-#include "zxnftp.h"
-
-void cmd_mget(char **params) {
-  // send command exit
-  // rx ok
-  // quit
-  return;
-}
--- zxnftp/client/cmd_mput.c
+++
@@ -1,8 +0,0 @@
-#include "zxnftp.h"
-
-void cmd_mput(char **params) {
-  // send command exit
-  // rx ok
-  // quit
-  return;
-}
