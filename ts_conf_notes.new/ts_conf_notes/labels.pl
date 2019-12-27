4a643808d394d4696f321380fb6247d42b02f26b
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-12-26 22:11:49 -0500

    match sources finished (hopefully)

---
+++ ts_conf_notes.new/ts_conf_notes/labels.pl
@@ -0,0 +1,13 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# labels from external_latex_labels array.
+
+
+1;
+
