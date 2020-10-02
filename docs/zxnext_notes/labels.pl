b9dc8faa08b59d9f5f8836d32b06c2670497d857
Theodore (Alex) Evans <taevans@cs.unm.edu>
2020-10-02 08:59:13 -0400

    updated docs (register 6F)

--- docs/zxnext_notes/labels.pl
+++ docs/zxnext_notes/labels.pl
@@ -1,4 +1,4 @@
-# LaTeX2HTML 2019 (Released January 1, 2019)
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
 # Associate labels original text with physical files.
 
 
@@ -17,7 +17,7 @@ $noresave{$key} = "$nosave";
 1;
 
 
-# LaTeX2HTML 2019 (Released January 1, 2019)
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
 # labels from external_latex_labels array.
 
 
